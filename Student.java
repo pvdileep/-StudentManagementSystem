5c7e6a471498be1740bb30aa7779f0acfa209d03
POLISETTI VEERA DILEEP <196k1a0563@gmail.com>
2026-06-22 23:46:06 +0530

    Added Student class

--- Student.java
+++ Student.java
@@ -1,1 +1,20 @@
+class Student {
+    int id;
+    String name;
+    int age;
+    double marks;
 
+    Student(int id, String name, int age, double marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "ID: " + id +
+               ", Name: " + name +
+               ", Age: " + age +
+               ", Marks: " + marks;
+    }
+}
